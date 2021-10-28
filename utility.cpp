565feb229311c7c9af67add6529eab12b6976979
Christof Arnosti <charno@charno.ch>
2021-10-28 23:02:18 +0200

    First commit

---
+++ utility.cpp
@@ -0,0 +1,101 @@
+#include "utility.h"
+#include <Preferences.h>
+#include <WiFi.h>
+
+
+using namespace charno::utility;
+
+String WiFiUtil::ssid = "";
+String WiFiUtil::password = "";
+int32_t WiFiUtil::channel = 0;
+uint8_t WiFiUtil::bssid[6] = {0,0,0,0,0,0};
+uint32_t WiFiUtil::wifiTimeoutMillis = 10000;
+String WiFiUtil::hostname = "";
+bool WiFiUtil::staticIp = false;
+String WiFiUtil::ip = "";
+String WiFiUtil::subnet = "";
+String WiFiUtil::gateway = "";
+String WiFiUtil::nameserver = "";
+bool WiFiUtil::doNtp = false;
+String WiFiUtil::ntpserver = "";
+const uint8_t WiFiUtil::empty_bssid[6] = {0,0,0,0,0,0};
+
+LogUtil::LogLevel LogUtil::logLevel = LogUtil::LogLevel::DEBUG;
+
+String TimeUtil::getTimeString()
+{
+    time_t tp = time(nullptr);
+    tm* timeinfo = localtime(&tp);
+
+    char tmbuf[64];
+    strftime(tmbuf, sizeof tmbuf, "%A, %B %d %Y %H:%M:%S", timeinfo);
+    return String(tmbuf);
+}
+
+void LogUtil::log(String message, String level, bool append){
+    if(Serial)
+    if(!append) {
+        Serial.print("\n[" + TimeUtil::getTimeString() + "] " + level + ": ");
+    }
+    Serial.print(message);
+}
+
+void WiFiUtil::getNtpTime()
+{
+    LogUtil::info("Get time by NTP: ");
+    configTime(0, 0, ntpserver.c_str());
+    tm timeinfo;
+    getLocalTime(&timeinfo);
+    LogUtil::info("done: " + TimeUtil::getTimeString(), true);
+}
+
+bool WiFiUtil::startWifi()
+{
+    if(WiFi.status() == WL_CONNECTED)
+        return true;
+
+    auto starttime = millis();
+    LogUtil::info(String("Connecting to ") + ssid);
+
+    // Set hostname if provided
+    if(hostname != "")
+    {
+        WiFi.setHostname(WiFiUtil::hostname.c_str());
+    }
+
+    // Set static IP if provided
+    if(staticIp)
+    {
+        IPAddress _ip; if(!_ip.fromString(ip)) _ip = IPAddress();
+        IPAddress _gateway; if(!_ip.fromString(gateway)) _gateway = IPAddress();
+        IPAddress _subnet; if(!_subnet.fromString(subnet)) _subnet = IPAddress();
+        IPAddress _dns; if(!_dns.fromString(nameserver)) _dns = IPAddress();
+
+        WiFi.config(_ip, _gateway, _subnet, _dns);
+    }
+
+    WiFi.begin(ssid.c_str(), password.c_str(), channel, memcmp(bssid, empty_bssid, 6) ? bssid : nullptr);
+
+    while(WiFi.status() != WL_CONNECTED && starttime + WiFiUtil::wifiTimeoutMillis > millis())
+    {
+        delay(100);
+        LogUtil::info(".", true);
+    }
+
+    if(WiFi.status() != WL_CONNECTED)
+    {
+        LogUtil::error("Could not connect to WIFI!");
+        WiFi.disconnect();
+        return false;
+    }
+
+    LogUtil::info(" CONNECTED! IP: ", true);
+    LogUtil::info(WiFi.localIP().toString(), true);
+
+    if(doNtp)
+    {
+        getNtpTime();
+    }
+    
+    return true;
+}
---
+++ utility.h
@@ -0,0 +1,99 @@
+#pragma once
+#include <Arduino.h>
+
+namespace charno 
+{ 
+    namespace utility 
+    {
+
+        class TimeUtil
+        {
+            public:
+                static String getTimeString();
+        };
+
+        class LogUtil
+        {
+            public:
+                enum LogLevel
+                {
+                    DEBUG = 0,
+                    INFO = 1,
+                    WARN = 2,
+                    ERROR = 3,
+                    NONE = 4,
+                };
+
+                static LogLevel logLevel;
+
+            private:
+                static void log(String message, String level, bool append);
+
+            public:
+                static void debug(String message, bool append = false)
+                {
+                    if(logLevel <= LogLevel::DEBUG) log(message, "DEBUG", append);
+                }
+
+                static void info(String message, bool append = false)
+                {
+                    if(logLevel <= LogLevel::INFO) log(message, " INFO", append);
+                }
+
+                static void warn(String message, bool append = false)
+                {
+                    if(logLevel <= LogLevel::WARN) log(message, " WARN", append);
+                }
+
+                static void error(String message, bool append = false)
+                {
+                    if(logLevel <= LogLevel::ERROR) log(message, "ERROR", append);
+                }
+
+        }; // class Logger
+
+        class WiFiUtil
+        {
+            public:
+                // Mandatory WiFi settings
+                static String ssid;
+                static String password;
+
+                // Optional WiFi settings
+                static int32_t channel;
+                static uint8_t bssid[6];
+                static uint32_t wifiTimeoutMillis;
+
+                // Hostname settings
+                static String hostname;
+
+                // Static IP settings
+                static bool staticIp;
+                static String ip;
+                static String subnet;
+                static String gateway;
+                static String nameserver;
+
+                // ntp settings
+                static bool doNtp;
+                static String ntpserver;
+                
+
+
+            private:
+
+                static const uint8_t empty_bssid[];
+
+                static void getNtpTime();
+
+            public:
+                static bool writeSettingsToFlash();
+
+                static bool readSettingsFromFlash();
+
+                static bool startWifi();
+
+        }; // class WiFiUtil
+
+    } // namespace utility
+} // namespace charno
